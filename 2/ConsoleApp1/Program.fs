d2a943c204e183d8c19de382fb6410fa177bfc10
q1temp <vikysikkamyshek16@gmail.com>
2026-02-27 00:46:57 +0500

    Laboratornya1

---
+++ 2/ConsoleApp1/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+
+let rec proverkachetchicel number =
+    if number < 10 
+    then number % 2 = 0
+    else
+        let lastchiclo = number % 10
+        if lastchiclo % 2 = 0 then
+            proverkachetchicel (number / 10)
+        else
+            false
+
+[<EntryPoint>]
+let main args = 
+    printfn "Введите число:"
+    let number = abs(int(Console.ReadLine())) // Берем модуль для отрицательных чисел
+    
+    let result = proverkachetchicel number
+    printfn "Число %d состоит только из четных чисел: %b" number result
+    0
---
+++ ConsoleApp1/ConsoleApp1/Program.fs
@@ -0,0 +1,8 @@
+﻿let append list1 = 
+    let list1 = ["List.map"; "List.filter"; "List.fold"; "List.iter"; "List.length"; "List.append"; "List.sort"; "List.head"; "List.tail"; "List.contains"; "List.zip"]
+    list1  
+
+[<EntryPoint>]
+let main args = 
+    printfn "Список из названия функций модуля List : %A" (append 0) 
+    0
