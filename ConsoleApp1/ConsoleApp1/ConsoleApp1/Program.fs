44757ee32937d8d790ac56b016a70570643ce56d
q1temp <vikysikkamyshek16@gmail.com>
2026-03-06 11:36:57 +0500

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/ConsoleApp1/Program.fs
@@ -0,0 +1,22 @@
+﻿open System
+open System.Reflection
+
+let printListMethods () =
+    let t = typeof<int list>.Assembly.GetType("Microsoft.FSharp.Collections.ListModule")
+     
+    if t = null then
+            printfn "Модуль List не найден"
+        else
+            [
+                for m in t.GetMethods() do
+                    yield m.Name
+            ]
+            |> List.distinct
+            |> List.sort
+            |> List.iter (printfn "%s")
+
+[<EntryPoint>]
+let main argv = 
+    printListMethods()
+    0
+
